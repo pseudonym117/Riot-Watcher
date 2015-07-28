de136cace8deb2f5bc8f59b8cd18098466c9f60e
Josh <Qinusty@users.noreply.github.com>
2015-07-28 11:39:16 +0100

    More syntax changes after further testing.

--- riotwatcher/riotwatcher.py
+++ riotwatcher/riotwatcher.py
@@ -521,7 +521,7 @@ class RiotWatcher:
             **kwargs
         )
     def get_match_list(self, summoner_id, region=None, champion_ids=None, ranked_queues=None, seasons=None, 
-                        begin_time=None, end_time=None begin_index=None, end_index=None):
+                        begin_time=None, end_time=None, begin_index=None, end_index=None):
         return self._match_list_request(
             '{summoner_id}'.format(summoner_id=summoner_id),
             region,
