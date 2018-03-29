21e9334e5678ea814b41a8f65aba76498b499fe8
Darren Lin <lin.darren95@gmail.com>
2018-03-29 11:09:12 -0400

    Should address #84

--- riotwatcher/_apis/LeagueApiV3.py
+++ riotwatcher/_apis/LeagueApiV3.py
@@ -46,21 +46,6 @@ class LeagueApiV3(NamedEndpoint):
             '/lol/league/v3/masterleagues/by-queue/{queue}'.format(queue=queue)
         )
 
-    def by_summoner(self, region, summoner_id):
-        """
-        Get leagues in all queues for a given summoner ID
-
-        :param string region: the region to execute this request on
-        :param long summoner_id: the summoner ID to query
-
-        :returns: Set[LeagueListDTO]
-        """
-        return self._request(
-            self.by_summoner.__name__,
-            region,
-            '/lol/league/v3/leagues/by-summoner/{summonerId}'.format(summonerId=summoner_id)
-        )
-
     def by_id(self, region, league_id):
         """
         Get league with given ID, including inactive entries
