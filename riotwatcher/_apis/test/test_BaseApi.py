8d79a8f0d72f5ddcd605c3bc50d051b0ce467632
Simone Masiero <simone@duiker101.net>
2018-08-20 12:37:51 +0200

    changed tests for BaseApi

--- riotwatcher/_apis/test/test_BaseApi.py
+++ riotwatcher/_apis/test/test_BaseApi.py
@@ -3,28 +3,47 @@ import sys
 
 if sys.version_info > (3, 0):
     from unittest.mock import MagicMock
+    from unittest import mock
 else:
     from mock import MagicMock
+    import mock
 from .. import BaseApi
 
 
 class BaseApiTestCase(unittest.TestCase):
     def setUp(self):
-        self._expected_return = object()
+        self._expected_preview_return = object()
+        self._expected_after_return = object()
 
+        # Mock with returns in the handler
         self._request_handler_mock = MagicMock(name='request_handler')
+
         self._request_handler_mock.preview_request = MagicMock(name='preview_request')
-        self._request_handler_mock.preview_request.return_value = self._expected_return
+        self._request_handler_mock.preview_request.return_value = self._expected_preview_return
         self._request_handler_mock.after_request = MagicMock(name='after_request')
-        self._request_handler_mock.after_request.return_value = self._expected_return
+        self._request_handler_mock.after_request.return_value = self._expected_after_return
+
         self._request_handler_mock.preview_static_request = MagicMock(name='preview_static_request')
-        self._request_handler_mock.preview_static_request.return_value = self._expected_return
+        self._request_handler_mock.preview_static_request.return_value = self._expected_preview_return
         self._request_handler_mock.after_static_request = MagicMock(name='after_static_request')
-        self._request_handler_mock.after_static_request.return_value = self._expected_return
+        self._request_handler_mock.after_static_request.return_value = self._expected_after_return
+
+        # Mock without returns in the handler
+        self._request_handler_mock_no_return = MagicMock(name='request_handler')
+        self._request_handler_mock_no_return.preview_request = MagicMock(name='preview_request')
+        self._request_handler_mock_no_return.after_request = MagicMock(name='after_request')
+        self._request_handler_mock_no_return.preview_static_request = MagicMock(name='preview_static_request')
+        self._request_handler_mock_no_return.after_static_request = MagicMock(name='after_static_request')
+        self._request_handler_mock_no_return.preview_request.return_value = None
+        self._request_handler_mock_no_return.after_request.return_value = None
+        self._request_handler_mock_no_return.preview_static_request.return_value = None
+        self._request_handler_mock_no_return.after_static_request.return_value = None
 
         self._api_key = 'sadf'
 
-    def test_base_api_request(self):
+        self._mock_api_response = 'api_response_xx'
+
+    def test_base_api_request_preview_handler(self):
         base_api = BaseApi(self._api_key, [self._request_handler_mock])
         endpoint_name = 'endpoint_xx'
         method_name = 'method_xx'
@@ -42,50 +61,99 @@ class BaseApiTestCase(unittest.TestCase):
             {'extra': kwarg}
         )
 
+    def test_base_api_request_after_handler_with_preview_return(self):
+        base_api = BaseApi(self._api_key, [self._request_handler_mock])
+        endpoint_name = 'endpoint_xx'
+        method_name = 'method_xx'
+        region = 'region_xx'
+        url_ext = 'url_xx'
+        kwarg = 'extra_xx'
+
+        ret = base_api.request(endpoint_name, method_name, region, url_ext, extra=kwarg)
+
         self._request_handler_mock.after_request.assert_called_once_with(
             region,
             endpoint_name,
             method_name,
             'https://region_xx.api.riotgames.com%s' % url_ext,
-            self._expected_return
+            self._expected_preview_return
         )
 
-    def test_base_api_static_request(self):
-        base_api = BaseApi(self._api_key, [self._request_handler_mock])
-        version = 'version_xx'
-        locale = 'locale_xx'
-        url_ext = 'url_xx'
+        self.assertEqual(ret, self._expected_after_return)
 
-        base_api.request_static(version, locale, url_ext)
+    @mock.patch('requests.get')
+    def test_base_api_request_after_handler_with_preview_no_return(self, mock_get):
+        mock_get.return_value = self._mock_api_response
 
-        self._request_handler_mock.preview_static_request.assert_called_once_with(
-            version,
-            locale,
-            'http://ddragon.leagueoflegends.com/cdn/%s/data/%s/%s.json' % (version, locale, url_ext),
-        )
-
-        self._request_handler_mock.after_static_request.assert_called_once_with(
-            version,
-            locale,
-            'http://ddragon.leagueoflegends.com/cdn/%s/data/%s/%s.json' % (version, locale, url_ext),
-            self._expected_return
-        )
-
-    def test_base_api_version_request(self):
-        base_api = BaseApi(self._api_key, [self._request_handler_mock])
+        base_api = BaseApi(self._api_key, [self._request_handler_mock_no_return])
+        endpoint_name = 'endpoint_xx'
+        method_name = 'method_xx'
         region = 'region_xx'
+        url_ext = 'url_xx'
+        kwarg = 'extra_xx'
 
-        base_api.request_version(region)
+        ret = base_api.request(endpoint_name, method_name, region, url_ext, extra=kwarg)
 
-        self._request_handler_mock.preview_static_request.assert_called_once_with(
-            '',
-            '',
-            'https://ddragon.leagueoflegends.com/realms/%s.json' % region
+        self._request_handler_mock_no_return.after_request.assert_called_once_with(
+            region,
+            endpoint_name,
+            method_name,
+            'https://region_xx.api.riotgames.com%s' % url_ext,
+            self._mock_api_response
         )
 
-        self._request_handler_mock.after_static_request.assert_called_once_with(
-            '',
-            '',
-            'https://ddragon.leagueoflegends.com/realms/%s.json' % region,
-            self._expected_return
+        mock_get.assert_called_once_with(
+            'https://region_xx.api.riotgames.com%s' % url_ext,
+            headers={'X-Riot-Token': self._api_key},
+            params={'extra': kwarg}
         )
+
+        self.assertEqual(ret, self._mock_api_response)
+
+
+
+
+
+
+
+
+
+    # def test_base_api_static_request(self):
+    #     base_api = BaseApi(self._api_key, [self._request_handler_mock])
+    #     version = 'version_xx'
+    #     locale = 'locale_xx'
+    #     url_ext = 'url_xx'
+    #
+    #     base_api.request_static(version, locale, url_ext)
+    #
+    #     self._request_handler_mock.preview_static_request.assert_called_once_with(
+    #         version,
+    #         locale,
+    #         'http://ddragon.leagueoflegends.com/cdn/%s/data/%s/%s.json' % (version, locale, url_ext),
+    #     )
+    #
+    #     self._request_handler_mock.after_static_request.assert_called_once_with(
+    #         version,
+    #         locale,
+    #         'http://ddragon.leagueoflegends.com/cdn/%s/data/%s/%s.json' % (version, locale, url_ext),
+    #         self._expected_return
+    #     )
+    #
+    # def test_base_api_version_request(self):
+    #     base_api = BaseApi(self._api_key, [self._request_handler_mock])
+    #     region = 'region_xx'
+    #
+    #     base_api.request_version(region)
+    #
+    #     self._request_handler_mock.preview_static_request.assert_called_once_with(
+    #         '',
+    #         '',
+    #         'https://ddragon.leagueoflegends.com/realms/%s.json' % region
+    #     )
+    #
+    #     self._request_handler_mock.after_static_request.assert_called_once_with(
+    #         '',
+    #         '',
+    #         'https://ddragon.leagueoflegends.com/realms/%s.json' % region,
+    #         self._expected_return
+    #     )
