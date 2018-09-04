081bb2087f2d09f68db9677d5c790638e02f0efd
pseudonym117 <godtheresnonamesleft@gmail.com>
2018-09-03 22:17:20 -0500

    pypy + jython support

---
+++ tests/conftest.py
@@ -0,0 +1,38 @@
+
+import datetime
+
+import pytest
+
+
+real_datetime_class = datetime.datetime
+
+class DatetimeSubclassMeta(type):
+    @classmethod
+    def __instancecheck__(mcs, obj):
+        return isinstance(obj, real_datetime_class)
+
+class BaseMockDatetime(real_datetime_class):
+    @classmethod
+    def now(cls):
+        if hasattr(cls, '_now') and cls._now is not None:
+            return cls._now
+        else:
+            return real_datetime_class.now()
+
+    @classmethod
+    def set_now(cls, datetime_value):
+        cls._now = datetime_value
+
+MockedDatetime = DatetimeSubclassMeta(
+    'datetime',
+    (BaseMockDatetime,),
+    {}
+)
+
+@pytest.fixture
+def mock_datetime(monkeypatch):
+    with monkeypatch.context() as ctx:
+        ctx.setattr(datetime, 'datetime', MockedDatetime)
+        yield
+
+        del MockedDatetime._now
