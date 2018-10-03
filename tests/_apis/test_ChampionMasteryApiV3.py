1251a42c96720ae8f1d84313b4a0923d8cea9fb9
pseudonym117 <godtheresnonamesleft@gmail.com>
2018-10-03 13:21:20 -0500

    refactored champion mastery api to use Endpoints

--- tests/_apis/test_ChampionMasteryApiV3.py
+++ tests/_apis/test_ChampionMasteryApiV3.py
@@ -13,7 +13,7 @@ class TestChampionMasteryApiV3(object):
     def test_by_summoner(self):
         mock_base_api = MagicMock()
         expected_return = object()
-        mock_base_api.request.return_value = expected_return
+        mock_base_api.raw_request.return_value = expected_return
 
         mastery = ChampionMasteryApiV3(mock_base_api)
         region = 'afas'
@@ -21,11 +21,12 @@ class TestChampionMasteryApiV3(object):
 
         ret = mastery.by_summoner(region, summoner_id)
 
-        mock_base_api.request.assert_called_once_with(
+        mock_base_api.raw_request.assert_called_once_with(
             ChampionMasteryApiV3.__name__,
             mastery.by_summoner.__name__,
             region,
-            '/lol/champion-mastery/v3/champion-masteries/by-summoner/15462'
+            'https://afas.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/15462',
+            {}
         )
 
         assert ret is expected_return
@@ -33,7 +34,7 @@ class TestChampionMasteryApiV3(object):
     def test_summoner_by_champion(self):
         mock_base_api = MagicMock()
         expected_return = object()
-        mock_base_api.request.return_value = expected_return
+        mock_base_api.raw_request.return_value = expected_return
 
         mastery = ChampionMasteryApiV3(mock_base_api)
         region = 'fsgs'
@@ -42,11 +43,12 @@ class TestChampionMasteryApiV3(object):
 
         ret = mastery.by_summoner_by_champion(region, summoner_id, champion_id)
 
-        mock_base_api.request.assert_called_once_with(
+        mock_base_api.raw_request.assert_called_once_with(
             ChampionMasteryApiV3.__name__,
             mastery.by_summoner_by_champion.__name__,
             region,
-            '/lol/champion-mastery/v3/champion-masteries/by-summoner/53526/by-champion/7'
+            'https://fsgs.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/53526/by-champion/7',
+            {}
         )
 
         assert ret is expected_return
@@ -54,7 +56,7 @@ class TestChampionMasteryApiV3(object):
     def test_scored_by_summoner(self):
         mock_base_api = MagicMock()
         expected_return = object()
-        mock_base_api.request.return_value = expected_return
+        mock_base_api.raw_request.return_value = expected_return
 
         mastery = ChampionMasteryApiV3(mock_base_api)
         region = 'fsgs'
@@ -62,11 +64,12 @@ class TestChampionMasteryApiV3(object):
 
         ret = mastery.scores_by_summoner(region, summoner_id)
 
-        mock_base_api.request.assert_called_once_with(
+        mock_base_api.raw_request.assert_called_once_with(
             ChampionMasteryApiV3.__name__,
             mastery.scores_by_summoner.__name__,
             region,
-            '/lol/champion-mastery/v3/scores/by-summoner/6243'
+            'https://fsgs.api.riotgames.com/lol/champion-mastery/v3/scores/by-summoner/6243',
+            {}
         )
 
         assert ret is expected_return
