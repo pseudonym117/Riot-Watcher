1ae7292c743b6cea987412ff6f38f6614c50f610
pseudonym117 <godtheresnonamesleft@gmail.com>
2018-12-05 21:15:30 -0600

    Third party code endpoint refectored; integration tests added (#99)

---
+++ src/riotwatcher/_apis/urls/ThirdPartyCodeApiV3Urls.py
@@ -0,0 +1,12 @@
+
+from . import RegionEndpoint
+
+
+class ThirdPartyCodeEndpoint(RegionEndpoint):
+    def __init__(self, url, **kwargs):
+        nurl = '/platform/v3/third-party-code' + url
+        super(ThirdPartyCodeEndpoint, self).__init__(nurl, **kwargs)
+
+
+class ThirdPartyCodeApiV3Urls(object):
+    by_summoner = ThirdPartyCodeEndpoint('/by-summoner/{summoner_id}')
--- src/riotwatcher/_apis/urls/__init__.py
+++ src/riotwatcher/_apis/urls/__init__.py
@@ -8,3 +8,4 @@ from .LolStatusApiV3Urls import LolStatusApiV3Urls
 from .MatchApiV3Urls import MatchApiV3Urls
 from .SpectatorApiV3Urls import SpectatorApiV3Urls
 from .SummonerApiV3Urls import SummonerApiV3Urls
+from .ThirdPartyCodeApiV3Urls import ThirdPartyCodeApiV3Urls
