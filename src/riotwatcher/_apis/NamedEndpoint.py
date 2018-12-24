3bdd77113ab7e7b7147ed96db3339a2834ec81be
Zachy24 <Zachary.Howard@Outlook.com>
2018-12-24 00:11:29 +0000

    Revert more inconsistencies

--- src/riotwatcher/_apis/NamedEndpoint.py
+++ src/riotwatcher/_apis/NamedEndpoint.py
@@ -1,23 +1,23 @@
 class NamedEndpoint(object):
     """
-    Helper class to inject endpoint name into calls to a HTTPClient instance without
+    Helper class to inject endpoint name into calls to a BaseApi instance without
     the child class explicitly adding the name every time.
     """
 
-    def __init__(self, http_client, endpoint_name):
+    def __init__(self, base_api, endpoint_name):
         """
-        Initialize a new NamedEndpoint which uses the provided http_client and
+        Initialize a new NamedEndpoint which uses the provided base_api and
         injects the provided endpoint_name into calls to _request
 
-        :param HTTPClient http_client:     the root API object to use for making all requests.
+        :param BaseApi base_api:     the root API object to use for making all requests.
         :param string endpoint_name: the name of the child endpoint
         """
-        self._http_client = http_client
+        self._base_api = base_api
         self._endpoint_name = endpoint_name
 
     async def _raw_request(self, method_name, region, url, query_params):
         """
-        Sends a request through the HTTPClient instance provided, injecting the provided endpoint_name
+        Sends a request through the BaseApi instance provided, injecting the provided endpoint_name
         into the method call, so the caller doesn't have to.
 
         :param string method_name:  The name of the calling method
@@ -25,6 +25,6 @@ class NamedEndpoint(object):
         :param string url:          The full URL to the method being requested.
         :param dict query_params:   Query parameters to be provided in the HTTP request
         """
-        return await self._http_client._request(
+        return await self._base_api._request(
             self._endpoint_name, method_name, region, url, query_params
         )
