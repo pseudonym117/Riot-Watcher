1aa179eae4106c92678d96496bc40ba2107add06
AG Stephan <godtheresnonamesleft@gmail.com>
2019-01-06 01:16:16 -0600

    made file names consistent

--- src/riotwatcher/_apis/urls/__init__.py
+++ src/riotwatcher/_apis/urls/__init__.py
@@ -1,10 +1,10 @@
 from .Endpoint import Endpoint, RegionEndpoint
 
-from .ChampionApiV3Urls import ChampionApiV3Urls
+from .ChampionApiUrls import ChampionApiV3Urls
 from .ChampionMasteryApiUrls import ChampionMasteryApiV3Urls, ChampionMasteryApiV4Urls
 from .DataDragonUrls import DataDragonUrls
 from .LeagueApiUrls import LeagueApiV3Urls, LeagueApiV4Urls
-from .LolStatusApiV3Urls import LolStatusApiV3Urls
+from .LolStatusApiUrls import LolStatusApiV3Urls
 from .MatchApiUrls import MatchApiV3Urls, MatchApiV4Urls
 from .SpectatorApiUrls import SpectatorApiV3Urls, SpectatorApiV4Urls
 from .SummonerApiUrls import SummonerApiV3Urls, SummonerApiV4Urls
