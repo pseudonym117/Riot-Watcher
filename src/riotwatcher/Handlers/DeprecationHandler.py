5d454e244c070931545200537b645fa1618ec6b1
AG Stephan <godtheresnonamesleft@gmail.com>
2019-05-07 22:01:01 -0500

    fixed code analysis warning

--- src/riotwatcher/Handlers/DeprecationHandler.py
+++ src/riotwatcher/Handlers/DeprecationHandler.py
@@ -14,19 +14,17 @@ class DeprecationHandler(RequestHandler):
         deprecation = response.headers.get("X-Riot-Deprecated")
         if deprecation is not None:
             try:
-                deprecation = int(deprecation)
-                deprecation = datetime.utcfromtimestamp(deprecation / 1000)
-
-                key = "{}.{}".format(endpoint_name, method_name)
-                if key not in self._warned:
-                    # technically race condition here, but worst case is we double log a warning
-                    self._warned.add(key)
-                    logging.warning(
-                        "Method %s has been deprecated by Riot! It will no longer work after %s",
-                        key,
-                        deprecation,
-                    )
-            except ValueError:
+                deprecation = datetime.utcfromtimestamp(int(deprecation) // 1000)
+            except (OSError, ValueError):
                 # API returned unexected value in header, so just do nothing
-                pass
+                return response
+            key = "{}.{}".format(endpoint_name, method_name)
+            if key not in self._warned:
+                # technically race condition here, but worst case is we double log a warning
+                self._warned.add(key)
+                logging.warning(
+                    "Method %s has been deprecated by Riot! It will no longer work after %s",
+                    key,
+                    deprecation,
+                )
         return response
