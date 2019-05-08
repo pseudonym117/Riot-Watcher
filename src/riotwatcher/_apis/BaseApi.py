c28a2c0ddd1063893243d6df85d75b61eb0f873c
pseudonym117 <godtheresnonamesleft@gmail.com>
2019-05-07 21:30:08 -0500

    remove dead code (#124)
    * remove dead code
    
    * removed more dead code

--- src/riotwatcher/_apis/BaseApi.py
+++ src/riotwatcher/_apis/BaseApi.py
@@ -40,13 +40,6 @@ class BaseApi(object):
 
         return response
 
-    def request(self, endpoint_name, method_name, region, url_ext, **kwargs):
-        url = "https://{region}.api.riotgames.com{ext}".format(
-            region=region, ext=url_ext
-        )
-
-        return self.raw_request(endpoint_name, method_name, region, url, kwargs)
-
     def raw_request_static(self, url, query_params):
         response = None
         early_ret_idx = None
@@ -54,32 +47,6 @@ class BaseApi(object):
         if self._request_handlers is not None:
             for idx, handler in enumerate(self._request_handlers, start=1):
                 response = handler.preview_static_request(url, query_params)
-                early_ret_idx = idx
-                if response is not None:
-                    break
-
-        if response is None:
-            response = requests.get(url)
-
-        if self._request_handlers is not None:
-            for handler in self._request_handlers[early_ret_idx:None:-1]:
-                mod = handler.after_static_request(url, response)
-                if mod is not None:
-                    response = mod
-
-        return response
-
-    def request_static(self, version, locale, url_ext):
-        url = "https://ddragon.leagueoflegends.com/cdn/{version}/data/{loc}/{ext}.json".format(
-            version=version, loc=locale, ext=url_ext
-        )
-
-        response = None
-        early_ret_idx = None
-
-        if self._request_handlers is not None:
-            for idx, handler in enumerate(self._request_handlers, start=1):
-                response = handler.preview_static_request(url, {})
                 early_ret_idx = idx
                 if response is not None:
                     break
