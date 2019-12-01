ab70996a3770a353950e5d99a35eb57183e68689
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-12-02 00:26:25 +0100

    Reformat files with 'black'

--- src/riotwatcher/Handlers/RateLimit/OopsRateLimiter.py
+++ src/riotwatcher/Handlers/RateLimit/OopsRateLimiter.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+
 log = logging.getLogger(__name__)
 
 
--- src/riotwatcher/Handlers/WaitingRateLimitHandler.py
+++ src/riotwatcher/Handlers/WaitingRateLimitHandler.py
@@ -1,4 +1,5 @@
 import logging
+
 log = logging.getLogger(__name__)
 
 from riotwatcher.Handlers.RateLimit import RateLimitHandler
