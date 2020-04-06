d7bf7ddc90bc993b124c6c9354cfbc35988b5342
AG Stephan <godtheresnonamesleft@gmail.com>
2020-04-06 14:24:44 -0500

    fix method names; add integration tests

--- tests/_apis/league_of_legends/test_ClashApiV1.py
+++ tests/_apis/league_of_legends/test_ClashApiV1.py
@@ -8,7 +8,7 @@ from riotwatcher._apis.league_of_legends import ClashApiV1
 @pytest.mark.lol
 @pytest.mark.unit
 class TestClashApiV1:
-    def test_by_summoner_id(self):
+    def test_by_summoner(self):
         mock_base_api = MagicMock()
         expected_return = object()
         mock_base_api.raw_request.return_value = expected_return
@@ -17,11 +17,11 @@ class TestClashApiV1:
         region = "htr35ge"
         summoner_id = "98532"
 
-        ret = clash.by_summoner_id(region, summoner_id)
+        ret = clash.by_summoner(region, summoner_id)
 
         mock_base_api.raw_request.assert_called_once_with(
             ClashApiV1.__name__,
-            clash.by_summoner_id.__name__,
+            clash.by_summoner.__name__,
             region,
             f"https://{region}.api.riotgames.com/lol/clash/v1/players/by-summoner/{summoner_id}",
             {},
@@ -29,7 +29,7 @@ class TestClashApiV1:
 
         assert ret is expected_return
 
-    def test_by_team_id(self):
+    def test_by_team(self):
         mock_base_api = MagicMock()
         expected_return = object()
         mock_base_api.raw_request.return_value = expected_return
@@ -38,11 +38,11 @@ class TestClashApiV1:
         region = "htr35ge"
         team_id = "psesn886"
 
-        ret = clash.by_team_id(region, team_id)
+        ret = clash.by_team(region, team_id)
 
         mock_base_api.raw_request.assert_called_once_with(
             ClashApiV1.__name__,
-            clash.by_team_id.__name__,
+            clash.by_team.__name__,
             region,
             f"https://{region}.api.riotgames.com/lol/clash/v1/teams/{team_id}",
             {},
@@ -70,7 +70,7 @@ class TestClashApiV1:
 
         assert ret is expected_return
 
-    def test_tournament_by_team_id(self):
+    def test_tournament_by_team(self):
         mock_base_api = MagicMock()
         expected_return = object()
         mock_base_api.raw_request.return_value = expected_return
@@ -79,11 +79,11 @@ class TestClashApiV1:
         region = "htr35ge"
         team_id = "25979"
 
-        ret = clash.tournament_by_team_id(region, team_id)
+        ret = clash.tournament_by_team(region, team_id)
 
         mock_base_api.raw_request.assert_called_once_with(
             ClashApiV1.__name__,
-            clash.tournament_by_team_id.__name__,
+            clash.tournament_by_team.__name__,
             region,
             f"https://{region}.api.riotgames.com/lol/clash/v1/tournaments/by-team/{team_id}",
             {},
@@ -91,7 +91,7 @@ class TestClashApiV1:
 
         assert ret is expected_return
 
-    def test_by_tournament_id(self):
+    def test_by_tournament(self):
         mock_base_api = MagicMock()
         expected_return = object()
         mock_base_api.raw_request.return_value = expected_return
@@ -100,11 +100,11 @@ class TestClashApiV1:
         region = "htr35ge"
         tournament_id = "25979"
 
-        ret = clash.by_tournament_id(region, tournament_id)
+        ret = clash.by_tournament(region, tournament_id)
 
         mock_base_api.raw_request.assert_called_once_with(
             ClashApiV1.__name__,
-            clash.by_tournament_id.__name__,
+            clash.by_tournament.__name__,
             region,
             f"https://{region}.api.riotgames.com/lol/clash/v1/tournaments/{tournament_id}",
             {},
