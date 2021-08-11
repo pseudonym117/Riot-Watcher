9a419f05ee35cd68bb4c1dc8a144fc63871656c7
AG Stephan <godtheresnonamesleft@gmail.com>
2021-08-11 01:28:46 -0500

    add coverage

--- tests/test_LolWatcher.py
+++ tests/test_LolWatcher.py
@@ -1,7 +1,12 @@
 import pytest
 
 from riotwatcher import LolWatcher
-from riotwatcher._apis.league_of_legends import MatchApiV4, MatchApiV5
+from riotwatcher._apis.league_of_legends import (
+    MatchApiV4,
+    MatchApiV5,
+    LolStatusApiV3,
+    LolStatusApiV4,
+)
 
 
 @pytest.mark.lol
@@ -31,3 +36,15 @@ class TestLolWatcher:
     def test_uses_match_v5_when_true(self):
         watcher = LolWatcher(api_key="RGAPI-this-is-a-fake", default_match_v5=True)
         assert isinstance(watcher.match, MatchApiV5)
+
+    def test_defaults_status_v3(self):
+        watcher = LolWatcher(api_key="RGAPI-this-is-a-fake")
+        assert isinstance(watcher.lol_status, LolStatusApiV3)
+
+    def test_uses_status_v3_when_false(self):
+        watcher = LolWatcher(api_key="RGAPI-this-is-a-fake", default_status_v4=False)
+        assert isinstance(watcher.lol_status, LolStatusApiV3)
+
+    def test_uses_status_v4_when_true(self):
+        watcher = LolWatcher(api_key="RGAPI-this-is-a-fake", default_status_v4=True)
+        assert isinstance(watcher.lol_status, LolStatusApiV4)
